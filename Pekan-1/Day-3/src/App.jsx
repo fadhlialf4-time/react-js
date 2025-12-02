99eb361aa5b21c1d80b754fa0334b0e0f52d640c
fdliiialffajri <fadhlialf4@gmail.com>
2025-12-02 13:33:51 +0700

    initial commit

---
+++ Pekan-1/Day-3/src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from "./component/greeting"
+// import Welcome from "./component/Welcome"
+
+function App() {
+  return (
+  <div>
+  <Greeting/>
+  <p>Ini adalah paragraf tentang React</p>
+  </div>
+  )
+}
+
+export default App;
---
+++ Pekan-1/Day-3/src/component/Greeting.jsx
@@ -0,0 +1,15 @@
+impot React from 'react';
+
+function Greeting() {
+    return <h1>Halo, Dunia!</h1>
+}
+
+export default Greeting;
+
+function Greeting() {
+    return <h1>Halo, Dunia!</h1>;
+}
+
+export default Greeting
+
+// export default 
